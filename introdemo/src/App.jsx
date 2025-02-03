572cd368fe9ada70d0090838ce322861211abe3a
Berniq <berniq1996@gmail.com>
2025-02-03 12:02:17 +0200

    updated app by including objects management in react

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -6,7 +6,6 @@ const Hello = (props) => {
   return (
     <div>
       <p>
-
         Hello {props.name}, you are {props.age} years old
       </p>
     </div>
@@ -17,11 +16,16 @@ const App = () => {
 
   const name = 'Peter'
   const age = 10
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
 
   return (
     <div>
       <h1>Greetings</h1>
-
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
       <Hello name='Maya' age={26 + 10} />
       <Hello name={name} age={age} />
     </div>
