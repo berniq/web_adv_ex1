58be596cf7fc8d6d9dd86bd9442510ea7e1d4c4a
Berniq <berniq1996@gmail.com>
2025-02-10 12:53:52 +0200

    part 2 comit

--- dbfile/backend/server.js
+++ dbfile/backend/server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
 
 const app = express();
 const port = 3000;
 
 // Middleware for handling JSON requests
 app.use(express.json());
-
-// Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to SQLite database
 const db = new sqlite3.Database('./database.db', (err) => {
@@ -41,11 +37,6 @@ app.post('/users', (req, res) => {
         }
         res.status(201).json({ id: this.lastID, name, email });
     });
-});
-
-// Serve HTML page at the root route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Fetch all users (Read)
